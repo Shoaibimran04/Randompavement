c8eca35b23a282fa855b3d2981fab23691680103
Shoaibimran04 <113394415+Shoaibimran04@users.noreply.github.com>
2023-04-04 13:54:59 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+const header = document.querySelector("header");
+window.addEventListener ("scroll", function(){
+    header.classList.toggle("sticky", window.scrolly > 100);
+
+});
